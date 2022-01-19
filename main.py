af4f3f7fa5b89208f4249c6f44be507c7221b2a2
melburmer <melihgorgulu98@gmail.com>
2022-01-19 14:49:24 +0100

    first_commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 15:23:22 2022
+
+@author: Melih
+"""
+# Importing Libraries
+import pandas as pd
+import numpy as np
+
+
+
+if __name__ == '__main__':
+    print('Hello World')
